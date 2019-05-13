ad05b9b152a330c6ab8a6ac49249ac82a89ea819
rj-90 <rj.codesstuff@gmail.com>
2019-05-13 11:12:46 -0700

    base game files
    uploading base game

---
+++ Assets/Scenes/Charecter.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//acts as a collector for all data types related to a charcter 
+public class Charecter : MonoBehaviour
+{
+    public string name;
+    public PhysicalCharectristcs pc;
+    public Data Data; // at this point read it from json 
+ 
+
+    public Charecter()
+    {
+        pc = new PhysicalCharectristcs();
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Data = FindObjectOfType<Data>();
+        //pc = Data.returnPhysicalCharectristcs();
+
+        pc = new PhysicalCharectristcs();
+        Debug.Log(pc.eyeColor);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    //public Charecter() {
+    //    if (pc != null)
+    //    {
+    //        pc = Data.returnPhysicalCharectristcs();
+    //    }
+    //    name = Data.returnAname();
+    //}
+}
---
+++ Assets/Scenes/Town.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Town : MonoBehaviour
+{
+
+    //public int numberOfNpcs = 10;
+    //public List<Charecter> charecters;
+    //// Start is called before the first frame update
+    /// 
+    /// 
+    Charecter test; 
+     void Start()
+    {
+        test = new Charecter();
+        if (test.pc.hasMark)
+        {
+            Debug.Log("the char has a scar");
+        }
+
+        //for (int i = 0; i <= numberOfNpcs; i++)
+        //{
+        //    charecters.Add(new Charecter());
+        //}
+    }
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+}
