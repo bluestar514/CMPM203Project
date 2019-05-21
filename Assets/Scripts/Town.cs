928ffd5ac196e33a280e34534a2275be1a573f46
rehafj <rj.codesstuff@gmail.com>
2019-05-20 22:10:03 -0700

    in progress - will fix this soon
    still in progress ---

--- Assets/Scripts/Town.cs
+++ Assets/Scripts/Town.cs
@@ -29,3 +29,101 @@ public class Town : MonoBehaviour
 
     //}
 }
+
+//in progress ---- 
+public class Map //kinda like a wrapper 
+{
+    List<Tile> tiles = new List<Tile>();
+    public Map() 
+    {
+
+    }
+
+    public void generateAMap()
+    {
+
+    }
+
+    //for modification on both npc and party members in a tile 
+    //todo fix this mess when you can focus :) 
+    public void PopulateSpecficTile(int tileID, List<Character> characters, List<Character> party)
+    {
+        tiles[tileID].populatedNPCs = characters;
+        tiles[tileID].populatedPatry = party;
+        //add method that populates sublist 
+    }
+    // for party memebers only 
+    public void PopulateSpecficTile(int tileID, List<Character> party)
+    {
+        tiles[tileID].populatedPatry = party;
+    }
+    //todo fix this mess when you can focus :) 
+    public List<Tile> populateAllTiles(List<Character> NPCcharacters, List<Character> party)
+    { 
+            //save npccharecters in a list manipulate it and then remove it instead of by ref
+           //tiles is supplied initially via constructor --- or can be sent here and then sent back 
+           point: // and repeat this loop 
+            foreach( Tile tile in tiles)
+            {
+                if ( tile.tileID == 0)// at the first tile then populate all party memebrs here 
+                {
+                    tile.populatedPatry = party; //full party 
+                }
+                //might change from call by refrence --- 
+            int x = Random.Range(0, NPCcharacters.Count);
+            tile.populatedNPCs.Add(NPCcharacters[x]);
+            NPCcharacters.Remove(NPCcharacters[x]);
+           
+        }
+
+        if (NPCcharacters.Count >= 1) // if my npc char list is not empty ( need to populate more then jump back to the bening ) 
+
+        {
+            goto point;
+        }
+
+        return tiles;
+    }
+
+
+}
+
+public class Tile
+{
+
+    //public List<Tile> subConnections = new List<Tile>();
+    public List<Character> populatedNPCs = new List<Character>();
+    public List<Character> populatedPatry = new List<Character>();
+    public int tileID = 0; // index 
+    string TitleType = "";
+
+
+    public Tile()
+    {
+
+    }
+    public Tile(List<Tile> connectingTiles,string TitleType)
+    {
+
+    }
+
+    public void PopulateTile(int tileID, List<Character> characters, List<Character> party)
+    {
+        populatedNPCs = characters;
+        populatedPatry = party;
+    }
+
+    public void populateNPCTile(List<Character> npc)
+    {
+
+    }
+
+    //`public Tile returnPopulatedTile()
+    //{
+
+    //    Tile x;
+    //    return x;
+    //}
+
+
+}
