f3b7b72e5cb3dd55b730836cc9bfa8c458c5776b
Beth <elaolive@ucsc.edu>
2019-05-22 17:08:29 -0700

    Made a distinction between Social and other kinds of actions
    Social is fairly well defined (doesn't take traits into consideration)
    Movement is stubbed in.
    Room for other kinds of actions still

--- Assets/Scripts/Action.cs
+++ Assets/Scripts/Action.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class Action //social
 {
-    public string name = "social";
-    public float baseDesire = .2f;
-    public float familyModifier = 1.5f;
-    public float friendlyModifier = 2f;
-    public float enemyModifier = -.75f;
+    public string name;
+    public float baseDesire;
+
+    public Action(string name, float baseDesire=.1f)
+    {
+        this.name = name;
+        this.baseDesire = baseDesire;
+    }
 
     public virtual void Enact(Character actor, Tile location, string target)
     {
@@ -16,6 +19,52 @@ public class Action //social
     }
 
     public virtual float Desire(Character actor, Tile location, string target)
+    {
+        float currentDesire = baseDesire;
+
+        return Mathf.Max(currentDesire, 0);
+
+    }
+
+    public virtual void FormMemory(Character actor, Tile location, string target)
+    {
+        actor.AddMemory(name);
+    }
+
+
+    protected Character FindInRoom(string target, Tile location)
+    {
+        List<Character> combinedVisitors = new List<Character>();
+        combinedVisitors.AddRange(location.populatedNPCs);
+        combinedVisitors.AddRange(location.populatedPatry);
+        
+
+        foreach (Character character in combinedVisitors) {
+            if (character.name == target) {
+                return character;
+            }
+        }
+        return null;
+    }
+}
+
+
+public class ActionSocial : Action {
+    public float familyModifier;
+    public float friendlyModifier;
+    public float enemyModifier;
+
+    public ActionSocial(string name, float baseDesire=.2f, float familyModifier=1.5f, float friendlyModifier=2f, float enemyModifier=-.75f): base(name, baseDesire)
+    {
+        this.name = name;
+        this.baseDesire = baseDesire;
+        this.familyModifier = familyModifier;
+        this.friendlyModifier = friendlyModifier;
+        this.enemyModifier = enemyModifier;
+    }
+
+
+    public override float Desire(Character actor, Tile location, string target)
     {
         float currentDesire = baseDesire;
 
@@ -31,26 +80,37 @@ public class Action //social
 
     }
 
-
-
-    public virtual void FormMemory(Character actor, Tile location, string target)
+    public override void FormMemory(Character actor, Tile location, string target)
     {
         actor.AddMemory(name + " with " + target);
     }
 
+}
 
-    Character FindInRoom(string target, Tile location)
+public class ActionMovement: Action {
+
+    public ActionMovement(string name, float baseDesire=.15f):base(name, baseDesire)
     {
-        List<Character> combinedVisitors = new List<Character>();
-        combinedVisitors.AddRange(location.populatedNPCs);
-        combinedVisitors.AddRange(location.populatedPatry);
-        
+        this.name = name;
+        this.baseDesire = baseDesire;
+    }
 
-        foreach (Character character in combinedVisitors) {
-            if (character.name == target) {
-                return character;
-            }
-        }
-        return null;
+    public override void Enact(Character actor, Tile location, string target)
+    {
+        base.Enact(actor, location, target);
+
+        //something that moves a character to target location
+    }
+
+    public override float Desire(Character actor, Tile location, string target)
+    {
+        //something that checks if the locations are connected and the path is traversable
+        //something that sees how much they might want to do that based on their traits
+
+        return 0; //replace later
+    }
+    public override void FormMemory(Character actor, Tile location, string target)
+    {
+        actor.AddMemory("Moved to " + target);
     }
 }
